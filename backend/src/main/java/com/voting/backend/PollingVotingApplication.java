5ef5140b9c82c68b0215bdf2c5ffafc34945d42a
Md Iqbal <iqbal.hossain@spectrum-bd.com>
2025-10-08 11:07:52 +0600

    beckend build up files added

---
+++ backend/src/main/java/com/voting/backend/PollingVotingApplication.java
@@ -0,0 +1,13 @@
+package com.voting.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingVotingApplication.class, args);
+	}
+
+}
